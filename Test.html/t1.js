ee611b852b61b2fee677a18377e5d38fa23cd3fe
Rohan403 <18btcse35@suiit.ac.in>
2022-01-02 18:45:51 +0530

    commit

---
+++ Test.html/t1.js
@@ -0,0 +1,11 @@
+var uname = document.querySelector("#uname");
+var uemail = document.querySelector("#uemail");
+var upassword = document.querySelector("#upassword");
+var umale = document.querySelector("#umale");
+var ufemale = document.querySelector("#female");
+function send() {
+console.log(uname.value);
+console.log(uemail.value);
+console.log(upassword.value);
+console.log(umale.value);
+console.log(ufemale.value);
