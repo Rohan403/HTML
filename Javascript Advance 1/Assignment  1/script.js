b7b39750e7fc0e4553e8bc5240ac774a33447ec2
Rohan403 <18btcse35@suiit.ac.in>
2022-01-14 12:10:12 +0530

    commit

--- Javascript Advance 1/Assignment  1/script.js
+++
@@ -1,8 +0,0 @@
-function thumbsup(){
-    let like = document.getElementById('id');
-    like.innerText = parseInt(like.innerText) + 1;
-}
-function thumbsdown(){
- let dislike = document.getElementById('id');
-    dislike.innerText = parseInt(dislike.innerText) - 1;
-}
--- Javascript Advance 1/January 13/script.js
+++
@@ -1,3 +0,0 @@
-for(i=0; i <5 ;i++){
-    document.write(array[i]);
-}
